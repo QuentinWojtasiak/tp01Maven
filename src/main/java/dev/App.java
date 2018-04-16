743fb15a8d5d5fec6e894c2f4159cfa72af169f9
quentinw <quent.wojtasiak@gmail.com>
2018-04-16 15:59:36 +0200

    ajout profil

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("( ° 3 ° )");
-        System.out.println(asciiArt);
+    	String titre = ResourceBundle.getBundle("application").getString("titre"); 
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	// affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
